8e56f77f19827dc9ceebd8408573c99499df9526
AI-ux2 <jehak93234@anlocc.com>
2025-04-10 14:04:55 +0100

    Update debugging.lua

--- debugging.lua
+++ debugging.lua
@@ -1,51 +1,13 @@
--- inventory.lua
-
-Item = {}
-Item.__index = Item
-
-function Item:new(name, type, quantity)
-    return setmetatable({ name = name, type = type, quantity = quantity }, self)
+function logHealth(npc)
+    print(npc.name .. "'s current health is: " .. npc.health)
 end
 
-Inventory = {}
-Inventory.__index = Inventory
-
-function Inventory:new()
-    return setmetatable({ items = {} }, self)
+function logPositionalData(npc)
+    print(npc.name .. " is located at: (" .. npc.position.x .. ", " .. npc.position.y .. ")")
 end
 
-function Inventory:addItem(item)
-    if self.items[item.name] then
-        self.items[item.name].quantity = self.items[item.name].quantity + item.quantity
-    else
-        self.items[item.name] = item
-    end
-    print(item.quantity .. " " .. item.name .. " added to inventory.")
-end
-
-function Inventory:removeItem(itemName, quantity)
-    if self.items[itemName] then
-        if self.items[itemName].quantity >= quantity then
-            self.items[itemName].quantity = self.items[itemName].quantity - quantity
-            if self.items[itemName].quantity == 0 then
-                self.items[itemName] = nil
-            end
-            print(quantity .. " " .. itemName .. " removed from inventory.")
-        else
-            print("Not enough " .. itemName .. " to remove.")
-        end
-    else
-        print(itemName .. " does not exist in inventory.")
-    end
-end
-
-function Inventory:tradeItem(otherInventory, itemName, quantity)
-    if self.items[itemName] and self.items[itemName].quantity >= quantity then
-        otherInventory:addItem(Item:new(itemName, self.items[itemName].type, quantity))
-        self:removeItem(itemName, quantity)
-    else
-        print("Not enough items to trade.")
-    end
+function toggleDebugMode(isDebug)
+    DEBUG_MODE = isDebug
 end
 
 function Inventory:listItems()
