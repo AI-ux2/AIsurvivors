a19a679f404ff4785822c6eeecb35642c64903d2
AI-ux2 <jehak93234@anlocc.com>
2025-04-10 12:57:00 +0100

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,10 @@
+-- settings.lua
+
+MAX_NPCS = 10
+SPAWN_RADIUS = 1000
+SAFE_ZONES = {
+    { x = 50, y = 50 },
+    { x = 200, y = 200 },
+    { x = 400, y = 400 },
+    -- Additional safe zones can be added
+}
