680eb9956e810f1b5dc94b5877da93b0fdee2133
AI-ux2 <jehak93234@anlocc.com>
2025-04-10 14:03:26 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,52 +1,24 @@
--- inventory.lua
+require 'npc'
+require 'behaviors'
+require 'pathfinding'
+require 'combat'
+require 'environment'
+require 'settings'
+require 'events'
+require 'relationships'
+require 'inventory'
+require 'quests'
+require 'factions'
+require 'group_behaviors'
+require 'stealth'
+require 'health'
 
-Item = {}
-Item.__index = Item
-
-function Item:new(name, type, quantity)
-    return setmetatable({ name = name, type = type, quantity = quantity }, self)
-end
-
-Inventory = {}
-Inventory.__index = Inventory
-
-function Inventory:new()
-    return setmetatable({ items = {} }, self)
-end
-
-function Inventory:addItem(item)
-    if self.items[item.name] then
-        self.items[item.name].quantity = self.items[item.name].quantity + item.quantity
-    else
-        self.items[item.name] = item
-    end
-    print(item.quantity .. " " .. item.name .. " added to inventory.")
-end
-
-function Inventory:removeItem(itemName, quantity)
-    if self.items[itemName] then
-        if self.items[itemName].quantity >= quantity then
-            self.items[itemName].quantity = self.items[itemName].quantity - quantity
-            if self.items[itemName].quantity == 0 then
-                self.items[itemName] = nil
-            end
-            print(quantity .. " " .. itemName .. " removed from inventory.")
-        else
-            print("Not enough " .. itemName .. " to remove.")
-        end
-    else
-        print(itemName .. " does not exist in inventory.")
-    end
-end
-
-function Inventory:tradeItem(otherInventory, itemName, quantity)
-    if self.items[itemName] and self.items[itemName].quantity >= quantity then
-        otherInventory:addItem(Item:new(itemName, self.items[itemName].type, quantity))
-        self:removeItem(itemName, quantity)
-    else
-        print("Not enough items to trade.")
-    end
-end
+function spawnNPCs()
+    globalNPCList = {} -- Global table to track NPCs
+    local numNPCs = 0
+    while numNPCs < MAX_NPCS do
+        local safeZone = SAFE_ZONES[math.random(1, #SAFE_ZONES)]
+        local randomX = safeZone.x
 
 function Inventory:listItems()
     for name, item in pairs(self.items) do
