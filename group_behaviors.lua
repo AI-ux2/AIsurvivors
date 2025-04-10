b72816085c79ee87800e38ba1593dd4739f67874
AI-ux2 <jehak93234@anlocc.com>
2025-04-10 14:06:01 +0100

    Update group_behaviors.lua

--- group_behaviors.lua
+++ group_behaviors.lua
@@ -1,10 +1,5 @@
--- group_behaviors.lua
-
 function NPCSurvivor:gatherResources()
-    print(self.name .. " is gathering resources as a group.")
-    -- Implement gathering logic, different behaviors based on group size
 end
 
 function NPCSurvivor:performGroupActivity(activity)
-    print(self.name .. " participates in group activity: " .. activity)
 end
