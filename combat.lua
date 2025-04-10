e740e04baf15d41c630bf013b553b812129cdc22
AI-ux2 <jehak93234@anlocc.com>
2025-04-10 14:04:17 +0100

    Update combat.lua

--- combat.lua
+++ combat.lua
@@ -1,55 +1,13 @@
--- inventory.lua
-
-Item = {}
-Item.__index = Item
-
-function Item:new(name, type, quantity)
-    return setmetatable({ name = name, type = type, quantity = quantity }, self)
-end
-
-Inventory = {}
-Inventory.__index = Inventory
-
-function Inventory:new()
-    return setmetatable({ items = {} }, self)
-end
-
-function Inventory:addItem(item)
-    if self.items[item.name] then
-        self.items[item.name].quantity = self.items[item.name].quantity + item.quantity
-    else
-        self.items[item.name] = item
+function NPCSurvivor:fight(zombie)
+    local damageToZombie = math.random(15, 25)
+    if math.random() < self.criticalHitChance then
+        damageToZombie = damageToZombie * 2
     end
-    print(item.quantity .. " " .. item.name .. " added to inventory.")
-end
 
-function Inventory:removeItem(itemName, quantity)
-    if self.items[itemName] then
-        if self.items[itemName].quantity >= quantity then
-            self.items[itemName].quantity = self.items[itemName].quantity - quantity
-            if self.items[itemName].quantity == 0 then
-                self.items[itemName] = nil
-            end
-            print(quantity .. " " .. itemName .. " removed from inventory.")
-        else
-            print("Not enough " .. itemName .. " to remove.")
-        end
-    else
-        print(itemName .. " does not exist in inventory.")
-    end
-end
+    local damageToNPC = math.random(5, 10)
+    self.health = self.health - damageToNPC
 
-function Inventory:tradeItem(otherInventory, itemName, quantity)
-    if self.items[itemName] and self.items[itemName].quantity >= quantity then
-        otherInventory:addItem(Item:new(itemName, self.items[itemName].type, quantity))
-        self:removeItem(itemName, quantity)
-    else
-        print("Not enough items to trade.")
-    end
-end
-
-function Inventory:listItems()
-    for name, item in pairs(self.items) do
-        print(item.quantity .. " x " .. name .. " (" .. item.type .. ")")
+    if self.health <= 0 then
+        self:die()
     end
 end
